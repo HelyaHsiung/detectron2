7700c56fac279e43d29e34c3f333612151d46892
Haiyang Xiong <69351632+HelyaHsiung@users.noreply.github.com>
2024-03-20 11:21:46 +0800

    add indexing parameter of torch.meshgrid

--- projects/DensePose/densepose/modeling/losses/cycle_pix2shape.py
+++ projects/DensePose/densepose/modeling/losses/cycle_pix2shape.py
@@ -24,7 +24,7 @@ def _create_pixel_dist_matrix(grid_size: int) -> torch.Tensor:
     # row = i // grid_size
     # col = i % grid_size
     pix_coords = (
-        torch.stack(torch.meshgrid(rows, cols), -1).reshape((grid_size * grid_size, 2)).float()
+        torch.stack(torch.meshgrid(rows, cols, indexing='ij'), -1).reshape((grid_size * grid_size, 2)).float()
     )
     return squared_euclidean_distance_matrix(pix_coords, pix_coords)
 
